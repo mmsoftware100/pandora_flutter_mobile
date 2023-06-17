c1beb7f1c03b412a6c38cadeaf0dc3ae9b3c3202
Aung Htet <48619157+AungAungHtet2019@users.noreply.github.com>
2023-06-17 20:00:51 +0630

    a

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,7 +1,43 @@
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../core/error/failures.dart';
+import '../domain/entities/user.dart';
+import '../domain/usecases/user_login.dart';
 
 class UserProvider extends ChangeNotifier{
 
+  //use case list
+  final UserLogin userLogin;
+
+  //constructor
+  UserProvider({
+    required this.userLogin;
+
+  });
+
+  // data repo
+  User user = User.sample;
+
+  //methods
+
+  Future<bool> login({required String accessToken}) async {
+    bool status = true;
+
+    final Either<Failure, User> userEither = await userLogin(UserLoginParams(accessToken: accessToken));
+    return userEither.fold(
+            (failure) {
+              print("UserProvider->login failure");
+              print(failure);
+              status = false;
+              return status;
+            },
+            (loggedInUser) {
+              user = loggedInUser;
+              status =  true;
+              return status;
+            });
+  }
   
 }
